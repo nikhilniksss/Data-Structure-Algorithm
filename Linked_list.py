3dc548bb7276b5f2efad9259782236e74b6c69e8
gituser <49320663+gituser945@users.noreply.github.com>
2024-01-15 00:51:41 +0530

    Add files via upload

---
+++ Linked_list.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # function to insert element at the beginning
+    
+    def insert_at_beginning(self,data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+
+    # function to insert element at the end
+    
+    def insert_at_end(self,data):
+        new_node = Node(data)
+        if self.head:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+
+    # function to implement search
+    
+    def search(self,data):
+        current_node = self.head
+        while current_node:
+            if current_node.data == data:
+                return True
+            else:
+                current_node = current_node.next
+        return False
+    
+sushi_preparation = LinkedList()
+sushi_preparation.insert_at_end("prepare")
+sushi_preparation.insert_at_end("roll")
+sushi_preparation.insert_at_beginning("assemble")
+
+sushi_preparation.search("roll")
+sushi_preparation.search("mixing")
+
+
