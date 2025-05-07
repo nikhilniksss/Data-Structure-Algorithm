8fe0ad9654272d2bc268b353c55e361b7ef57f78
Nikhil Kumar <49320663+nikhilniksss@users.noreply.github.com>
2025-05-08 02:00:56 +0530

    indentation

--- min_stack.py
+++ min_stack.py
@@ -8,7 +8,6 @@ class MinStack:
 
     def push(self,x):
         self.stack.append(x)
-
         if self.min:
             if x < self.min[-1]:
                 self.min.append(x)
@@ -18,7 +17,6 @@ class MinStack:
     def pop(self):
         if self.stack[-1] == self.min[-1]:
             self.min.pop()
-
         self.stack.pop()
 
 
