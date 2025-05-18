fe95346039b0ffd178b55bc9caa64bd60f72b0e1
Nikhil Kumar <49320663+nikhilniksss@users.noreply.github.com>
2025-05-19 01:56:33 +0530

    updated

--- Blind 75/4.anagram.py
+++ Blind 75/4.anagram.py
@@ -10,3 +10,31 @@
 # Input: s = "rat", t = "car"
 # Output: false
 
+
+def anagram_program(str1,str2):
+
+    str1 = str1.lower()
+    str2 = str2.lower()
+
+    str1 = str1.replace(" ","")
+    str2 = str2.replace(" ","")
+
+    count = [0] * 26
+
+    for char in str1:
+        count[ord(char) - ord('a')] += 1
+
+    for char in str2:
+        count[ord(char) - ord('a')] -= 1
+
+    for ele in count:
+        if ele != 0:
+            return False
+    return True
+
+
+s = "anagram", t = "nagaram"
+print(anagram_program(s,t))
+
+s = "rat", t = "car"
+print(anagram_program(s,t))
