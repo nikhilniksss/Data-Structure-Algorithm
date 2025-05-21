8e44cf713aa42c1aa9cb0ae5f80dc500d416ff98
Nikhil Kumar <49320663+nikhilniksss@users.noreply.github.com>
2025-05-22 03:29:30 +0530

    3sum

--- Blind 75/8.3sum.py
+++ Blind 75/8.3sum.py
@@ -26,3 +26,28 @@
 # Output: [[0,0,0]]
 # Explanation: The only possible triplet sums up to 0.
 
+def three_sum(nums):
+    if nums is None or len(nums) < 3:
+        return []
+    
+    nums.sort()
+
+    result = set()
+
+    for i in range(len(nums)-2):
+        left, right = i+1, len(nums)-1
+        while left < right:
+            target = nums[i] + nums[left]+nums[right]
+
+            if target == 0:
+                result.add((nums[i],nums[left],nums[right]))
+                left += 1
+                right -= 1
+            elif target < 0:
+                left += 1
+            elif target > 0:
+                right -= 1
+    return list(map(list,result))
+
+nums = [-1,0,1,2,-1,-4]
+print(three_sum(nums))
