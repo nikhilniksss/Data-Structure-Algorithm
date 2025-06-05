af9011952ac6758390ecb10213fc3a764a7b01f8
Nikhil Kumar <nikhil.1234.niks@gmail.com>
2025-06-05 15:36:45 +0530

    longest consecutive

--- Blind 75/16.longest consecutive sequence.py
+++ Blind 75/16.longest consecutive sequence.py
@@ -17,3 +17,27 @@
 # Input: nums = [1,0,1,2]
 # Output: 3
 
+def longest_sequence(nums):
+    numSet = set(nums)
+    longest = 0
+
+    for n in numSet:
+        if n - 1 not in numSet:
+            current_num = n
+            current_streak = 1
+
+            while current_num + 1 in numSet:
+                current_num += 1
+                current_streak += 1
+            longest = max(longest,current_streak)
+    return longest
+
+
+nums = [100,4,200,1,3,2]
+print(longest_sequence(nums))
+
+nums = [0,3,7,2,5,8,4,6,0,1]
+print(longest_sequence(nums))
+
+nums = [1,0,1,2]
+print(longest_sequence(nums))
