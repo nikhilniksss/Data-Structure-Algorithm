1bc3df2fdf57d284412b0613f8db31588b51575f
Nikhil Kumar <49320663+nikhilniksss@users.noreply.github.com>
2025-07-07 00:51:00 +0530

    is subsequence problem

---
+++ 1.Arrays & Hashing/is subsequence.py
@@ -0,0 +1,28 @@
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+# Example 1:
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
+
+# Example 2:
+# Input: s = "axc", t = "ahbgdc"
+# Output: false
+
+def is_subsequence(s,t):
+    i, j = 0, 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    return i == len(s)
+
+s = "abc"
+t = "ahbgdc"
+print(is_subsequence(s,t))
+
+s = "axc"
+t = "ahbgdc"
+print(is_subsequence(s,t))
