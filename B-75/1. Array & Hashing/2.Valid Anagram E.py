59130e7eaedc37e9742391a038c21789091da168
Nikhil Kumar <49320663+nikhilniksss@users.noreply.github.com>
2025-08-18 01:05:14 +0530

    valid anagram blin 75

---
+++ B-75/1. Array & Hashing/2.Valid Anagram E.py
@@ -0,0 +1,35 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# Example 1:
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+
+# Example 2:
+# Input: s = "rat", t = "car"
+# Output: false
+
+def valid_anagram(s,t):
+    if len(s) != len(t):
+        return False
+    
+    count = [0] * 26
+    for i in range(len(s)):
+        count[ord(s[i]) - ord("a")] += 1
+        count[ord(t[i]) - ord("a")] -= 1
+
+    for val in count:
+        if val != 0:
+            return False
+    return True
+
+s = "anagram"
+t = "nagaram"
+print(valid_anagram(s,t))
+
+s = "rat"
+t = "cat"
+print(valid_anagram(s,t))
+
+s = "racecar"
+t = "rraacce"
+print(valid_anagram(s,t))
