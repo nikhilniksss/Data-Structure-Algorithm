1adbaa91ebe5d0bbd9fcaeb78a680d0b968ccdcd
Nikhil Kumar <49320663+nikhilniksss@users.noreply.github.com>
2025-08-19 01:49:23 +0530

    practice

---
+++ practice set/practice1.py
@@ -0,0 +1,26 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Example 1:
+# Input: nums = [1,2,3,1]
+# Output: true
+# Explanation:
+# The element 1 occurs at the indices 0 and 3.
+
+def contains_duplicate(nums):
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        else:
+            seen.add(num)
+    return False
+
+nums = [1,2,3,1]
+print(contains_duplicate(nums))
+
+nums = [1,1,1,1]
+print(contains_duplicate(nums))
+
+nums = [1,2,3,4]
+print(contains_duplicate(nums))
+
