a7f72f48628d4c8473dc34837dfa39928a80a664
Nikhil Kumar <49320663+nikhilniksss@users.noreply.github.com>
2025-09-29 19:00:18 +0530

    Update 3.Linked List Cycle E.py

--- B-75/7.Linked List/3.Linked List Cycle E.py
+++ B-75/7.Linked List/3.Linked List Cycle E.py
@@ -26,3 +26,10 @@ Output: false
 Explanation: There is no cycle in the linked list.
 
 def linkedList(head):
+    fast,slow = head,head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if fast == slow:
+           return True
+    return False
